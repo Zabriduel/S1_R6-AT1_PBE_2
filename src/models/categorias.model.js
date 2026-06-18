ee998ef8568339e4fe69cb2aac13e182831c754e
Zabriduel <gabriel31alves@gmail.com>
2026-06-18 09:29:35 -0300

    fix: import database models

--- src/models/categorias.model.js
+++ src/models/categorias.model.js
@@ -1,33 +1,33 @@
-import pool from "../config/db.js";
+import {connection} from '../configs/Database.js';
 
 const categoriasModel = {
     selectALL: async () => {
         const sql = 'SELECT * FROM categorias;';
-        const [rows] = await pool.execute(sql)
+        const [rows] = await connection.execute(sql)
         return rows;
     },
     selectCategoria: async (pIdCategoria) => {
         const sql = 'SELECT * FROM categorias WHERE idCategoria = ?;';
         const values = [pIdCategoria];
-        const [rows] = await pool.execute(sql,values);
+        const [rows] = await connection.execute(sql,values);
         return rows;
     },
     insert: async (pDescricao) => {
         const sql = "INSERT INTO categorias(descricaoCategoria) VALUES (?);";
         const values = [pDescricao];
-        const [rows] = await pool.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     },
     update: async (pIdCategoria, pDescricao, ) => {
         const sql = "UPDATE categorias SET descricaoCategoria=? WHERE idCategoria=?;";
         const values = [pDescricao,pIdCategoria];
-        const [rows] = await pool.query(sql, values);
+        const [rows] = await connection.query(sql, values);
         return rows;
     },
     delete: async (pIdCategoria) => {
         const sql = "DELETE FROM categorias WHERE idCategoria = ?;";
         const values = [pIdCategoria];
-        const [rows] = await pool.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
 
     }
--- src/models/produtos.models.js
+++ src/models/produtos.models.js
@@ -1,33 +1,33 @@
-import pool from "../config/db.js";
+import {connection} from '../configs/Database.js';
 
 const produtosModels = {
     selectALL: async () => {
         const sql = 'SELECT *FROM produtos;';
-        const [rows] = await pool.execute(sql)
+        const [rows] = await connection.execute(sql)
         return rows;
     },
     selectProduto: async (pIdProduto) => {
         const sql = "SELECT * FROM produtos WHERE idProduto = ?";
         const values = [pIdProduto];
-        const [rows] = await pool.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     },
     insert: async (pIdCategoria, pNomeProduto, pValorProduto, pVinculoImagem) => {
         const sql = "INSERT INTO produtos(idCategoria,nomeProduto, valorProduto, vinculoImagem) VALUES (?,?,?,?);";
         const values = [pIdCategoria, pNomeProduto, pValorProduto, pVinculoImagem];
-        const [rows] = await pool.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     },
      update: async (pIdProduto, pIdCategoria, pNomeProduto, pValorProduto, pVinculoImagem) => {
         const sql = "UPDATE produtos SET idCategoria=?, nomeProduto=?, valorProduto=?, vinculoImagem=? WHERE idProduto=?;";
         const values = [pIdCategoria, pNomeProduto, pValorProduto, pVinculoImagem, pIdProduto];
-        const [rows] = await pool.query(sql, values);
+        const [rows] = await connection.query(sql, values);
         return rows;
     },
     delete: async (pIdProduto) => {
         const sql = "DELETE FROM produtos WHERE idProduto = ?;";
         const values = [pIdProduto];
-        const [rows] = await pool.execute(sql, values);
+        const [rows] = await connection.execute(sql, values);
         return rows;
     }
 }
